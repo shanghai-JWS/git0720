ee2d892d1fdc51498b23fce5efafae2b1c76d09c
JWS <JSW@shanghai.com>
2020-11-27 15:42:18 +0800

    first commit

---
+++ src/test/java/test0720.java
@@ -0,0 +1,12 @@
+/**
+ * @author jwsstart
+ * @create 2020-11-27 15:36
+ */
+public class test0720 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+    }
+
+
+}
